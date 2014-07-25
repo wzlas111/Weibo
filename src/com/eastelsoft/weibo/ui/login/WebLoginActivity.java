661706965822c5e585b619978d9e9ce1e022ea9b
wangzl <wzlas111@126.com>
2014-07-25 17:34:19 +0800

    src and res

--- src/com/eastelsoft/weibo/ui/login/WebLoginActivity.java
+++ src/com/eastelsoft/weibo/ui/login/WebLoginActivity.java
@@ -1,8 +1,12 @@
 package com.eastelsoft.weibo.ui.login;
 
 import android.app.ActionBar;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.eastelsoft.weibo.R;
 import com.eastelsoft.weibo.ui.base.AbstractAppActivity;
@@ -10,6 +14,8 @@ import com.eastelsoft.weibo.ui.base.AbstractAppActivity;
 public class WebLoginActivity extends AbstractAppActivity {
 	
 	private WebView webView;
+	
+	private MenuItem refreshItem; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,41 @@ public class WebLoginActivity extends AbstractAppActivity {
 		webView = (WebView)this.findViewById(R.id.webView);
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle(R.string.app_login);
+		webView.setWebViewClient(new WeiboWebClient());
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.actionbar_menu_weblogin, menu);
+		refreshItem = menu.findItem(R.id.menu_refresh);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private class WeiboWebClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			super.onPageStarted(view, url, favicon);
+		}
+		@Override
+		public void onReceivedError(WebView view, int errorCode,
+				String description, String failingUrl) {
+			super.onReceivedError(view, errorCode, description, failingUrl);
+		}
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+		}
+	}
+	
 }
---
+++ src/com/eastelsoft/weibo/utils/URLHelper.java
@@ -0,0 +1,6 @@
+package com.eastelsoft.weibo.utils;
+
+public class URLHelper {
+
+	public static final String TEST_URL = "http://58.240.63.104";
+}
